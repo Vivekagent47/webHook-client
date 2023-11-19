f1481937eae11fe83ccf213ca9c3b72a9de6409e
Vivek Chauhan <vivek.agent47@gmail.com>
2023-11-19 19:19:54 +0530

    formatter setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="grid place-items-center h-screen">
-      <Button>
-        Help Me
-      </Button>
+      <Button>Help Me</Button>
     </main>
-  )
+  );
 }
