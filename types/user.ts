34743e29da4cc745424e9ab084b135b2357985c4
Vivek Chauhan <vivek.agent47@gmail.com>
2023-12-08 18:45:09 +0530

    fix: update user types and add organization types

--- types/user.ts
+++ types/user.ts
@@ -1,3 +1,5 @@
+import { UserOrganization } from ".";
+
 export type User = {
   id: string;
   firstName: string;
@@ -7,9 +9,26 @@ export type User = {
   updatedAt: string;
 };
 
+export enum UserRole {
+  OWNER = "owner",
+  ADMIN = "admin",
+  MEMBER = "member",
+}
+
 export type UserRegisterData = {
   firstName: string;
   lastName: string;
   email: string;
   password: string;
+};
+
+export type AuthTokens = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+export type UserAuthData = {
+  user: User;
+  tokens: AuthTokens;
+  organizations: UserOrganization[];
 };
