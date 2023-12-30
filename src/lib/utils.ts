c5cba8a897b1282178e206e3c75d90b53d817d3b
Vivek Chauhan <vivek.agent47@gmail.com>
2023-12-30 21:54:40 +0530

    Add decodeToken function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function decodeToken(token: string) {
+  if (!token) {
+    return;
+  }
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace("-", "+").replace("_", "/");
+  const data = JSON.parse(window.atob(base64));
+  return data;
+}
