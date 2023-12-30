f95a475f63d1540c2bab5a06712c03a4e0a82be2
Vivek Chauhan <vivek.agent47@gmail.com>
2023-12-30 21:57:41 +0530

    chore: remove unnecessary comment

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import { router } from "@/router";
 import { useEffect } from "react";
 
 function App() {
-  // check if user is logged in
-  // if yes, fetch user data
-  // if no, redirect to login page
-  // if error, redirect to login page
-  // if loading, show loading screen
   useEffect(() => {
     const accessToken = localStorage.getItem("access-token");
     if (!accessToken) {
