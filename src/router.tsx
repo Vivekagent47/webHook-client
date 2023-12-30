d7a9f7f062bccb547e1505a68b6041fcaa73bc46
Vivek Chauhan <vivek.agent47@gmail.com>
2023-12-30 21:51:51 +0530

    Add Page404 component and update router configuration

--- src/router.tsx
+++ src/router.tsx
@@ -4,12 +4,14 @@ import Test from "@/pages/Test";
 import Auth from "@/pages/auth";
 import Login from "@/pages/auth/Login";
 import SignUp from "@/pages/auth/SignUp";
+import Page404 from "./pages/Page404";
 
 export const router = createBrowserRouter([
   {
     id: "root",
     path: "/",
     element: <Test />,
+    errorElement: <Page404 />,
   },
   {
     id: "login",
